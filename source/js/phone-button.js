abcf413336237845293d2d1afe357a0089f4977f
Evgeniy <casto2010@yandex.ru>
2020-08-17 00:15:36 +0300

    Добавил класс для того что бы кнопка не отображалась есои JS не загрузится

--- source/js/phone-button.js
+++ source/js/phone-button.js
@@ -1,6 +1,8 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+  navToggle.classList.remove('main-nav__toggle--nojs')
+
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('main-nav--opened');
   navMain.classList.toggle('main-nav--closed');
